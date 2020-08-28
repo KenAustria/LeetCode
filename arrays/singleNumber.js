02e0ec453c7e6f6d1a3786e0aa38ff23ae282ff0
Ken Austria <kenaustriadev@gmail.com>
2020-08-27 23:13:23 -0700

    singleNumber O(n)

--- arrays/singleNumber.js
+++ arrays/singleNumber.js
@@ -1,10 +1,10 @@
-var singleNumber = function(nums) {
-	var obj = {};
+let singleNumber = function(nums) {
+	let nums_hash = {};
 	for (var i = 0; i < nums.length; i++) {
-		nums[i] in obj ? delete obj[nums[i]] : obj[nums[i]] = 1
+		nums[i] in nums_hash ? delete nums_hash[nums[i]] : nums_hash[nums[i]] = 1
 	}
-	return Object.keys(obj)[0];
+	return Object.keys(nums_hash)[0];
 };
 
-console.log(singleNumber([2,2,1]));
-console.log(singleNumber([4,1,2,1,2]));
+console.log(singleNumber([2,2,1])); // 1
+console.log(singleNumber([4,1,2,1,2])); // 4
