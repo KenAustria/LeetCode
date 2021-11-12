0d34cadec8cceee94e6b18b16baae5f1b199b834
Ken Austria <kenaustriadev@gmail.com>
2021-11-11 17:36:15 -0800

    getConcatenation O(n) spread

--- arrays/getConcatenation.js
+++ arrays/getConcatenation.js
@@ -2,7 +2,5 @@
 // Time complexity: O(n)
 // Memory: 42.4MB
 const getConcatenation = (nums) => {
-	const newNums = [...nums]
-  const ans = nums.concat(newNums)
-  return ans
+	return [...nums, ...nums]
 }
