e8e6e3f5fd478742f696d202f9ca1035f36bfd86
Ken Austria <kenaustriadev@gmail.com>
2021-11-12 23:50:51 -0800

    buildArray O(n)

---
+++ arrays/buildArray.js
@@ -0,0 +1,9 @@
+// Runtime: 92ms
+// Time complexity: O(n)
+// Memory: 42.2MB
+const buildArray = () => {
+	return nums.map(i => nums[i]);
+}
+
+console.log(buildArray([0,2,1,5,3,4])); // [0,1,2,4,5,3]
+console.log(buildArray([5,0,1,2,3,4])) // [4,5,0,1,2,3]
