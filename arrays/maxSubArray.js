9ef8eb8005624e572329d7d238780c36206be29f
Ken Austria <kenaustriadev@gmail.com>
2022-04-13 10:45:10 +0900

    maxSubArray time=O(n) space=O(n)

--- arrays/maxSubArray.js
+++ arrays/maxSubArray.js
@@ -1,22 +1,23 @@
 // LINEAR
 // Runtime: 60ms
-// Time complexity: O(n)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 // Memory: 35.MB
 const maxSubArray = nums => {
-	// initialize current sum and maximum sum to -Infinity
-	let currSum = -Infinity;
-	let maxSum = -Infinity;
+  // initialize current sum and maximum sum to -Infinity
+  let currSum = -Infinity;
+  let maxSum = -Infinity;
 
-	for(let i = 0; i < nums.length; i++) {
-		// if current sum is below zero, assign zero as the current sum
-		currSum = Math.max(0, currSum);
-		// add current index value to current sum
-		currSum += nums[i];
-		// if current sum is greater than maximum sum, assing current sum to maximum sum
-		maxSum = Math.max(maxSum, currSum);
-	}
-	return maxSum;
-}
+  for (let i = 0; i < nums.length; i++) {
+    // if current sum is below zero, assign zero as the current sum
+    currSum = Math.max(0, currSum);
+    // add current index value to current sum
+    currSum += nums[i];
+    // if current sum is greater than maximum sum, assing current sum to maximum sum
+    maxSum = Math.max(maxSum, currSum);
+  }
+  return maxSum;
+};
 
 // [-2,1,-3,4,-1,2,1,-5,4]
 // currSum = 0, max = -∞
@@ -30,14 +31,14 @@ const maxSubArray = nums => {
 // i=7: currSum = 1 or -5 so we add it up and currSum=1 but max stays max=6
 // i=8: currSum = 5 or 4 so we add it up and currSum=5 but max stays 6
 
-console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]));
+console.log(maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 // Output: 6
 // Explanation: [4,-1,2,1] has the largest sum = 6
 
-console.log(maxSubArray([-2,-11,-13,-2,-14,-9,-5,-15,-3]));
+console.log(maxSubArray([-2, -11, -13, -2, -14, -9, -5, -15, -3]));
 // Output: -2
 // Explanation: [-2] has the largest sum = -2
 
-console.log(maxSubArray([-2,0,-1]));
+console.log(maxSubArray([-2, 0, -1]));
 // Output: 0
 // Explanation: [0] has the largest sum = 0
