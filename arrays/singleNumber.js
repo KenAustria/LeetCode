b6b5ef8ab7e4e1a6a9d6786e066697dc3767ec08
Ken Austria <kenaustriadev@gmail.com>
2022-05-02 22:59:16 +0900

    singleNumber time=O(n) space=O(1)

--- arrays/singleNumber.js
+++ arrays/singleNumber.js
@@ -1,10 +1,13 @@
-let singleNumber = function(nums) {
-	let nums_hash = {};
-	for (var i = 0; i < nums.length; i++) {
-		nums[i] in nums_hash ? delete nums_hash[nums[i]] : nums_hash[nums[i]] = 1
-	}
-	return Object.keys(nums_hash)[0];
+// Technique: Bit Manipulation
+// Runtime: 91ms
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// Memory: 43MB
+
+const singleNumber = nums => {
+  return nums.reduce((prev, curr) => prev ^ curr);
 };
 
-console.log(singleNumber([2,2,1])); // 1
-console.log(singleNumber([4,1,2,1,2])); // 4
+console.log(singleNumber([2, 2, 1])); // 1
+console.log(singleNumber([4, 1, 2, 1, 2])); // 4
+console.log(singleNumber([1])); // 1
