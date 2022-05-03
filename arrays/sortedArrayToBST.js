fb0a3b4b4ae97c8e1762052b45f6e8a843c3ba2d
Ken Austria <kenaustriadev@gmail.com>
2022-05-03 19:19:41 +0900

    sortedArrayToBST time=O(log n) space=O(n)

---
+++ arrays/sortedArrayToBST.js
@@ -0,0 +1,25 @@
+// Technique: Recursive
+// Runtime: 80ms
+// Time Complexity: O(log n)
+// Space Complexity: O(n)
+// Memory: 44.3MB
+
+var sortedArrayToBST = (nums, start = 0, end = nums.length - 1) => {
+  // since nums array is sorted ascendingly, start should always be <= to end
+  if (start <= end) {
+    // declare variables
+    let mid = Math.floor(nums.length / 2);
+    let root = new TreeNode(nums[mid]);
+
+    root.left = sortedArrayToBST(nums, start, mid);
+    root.right = sortedArrayToBST(nums, mid + 1, end);
+
+    return root;
+  }
+
+  // error check
+  return null;
+};
+
+console.log(sortedArrayToBST(-10, -3, 0, 5, 9)); // [0, -3, -10, null, 5]
+console.log(sortedArrayToBST([1, 3])); // [3, 1]
