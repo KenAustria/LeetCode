7500c2c0595edc58e5dff24f1ea078f6fe923253
Ken Austria <kenaustriadev@gmail.com>
2022-05-05 15:45:46 +0900

    majorityElement time=O(n) space=O(1)

--- arrays/majorityElement.js
+++ arrays/majorityElement.js
@@ -5,17 +5,17 @@
 // Memory: 43.5MB
 
 const majorityElement = nums => {
-	// declare host hash
-	let numsHash = {}
+  // declare host hash
+  let numsHash = {};
 
-	// populate host hash
-	for (let i = 0; i < nums.length; i++) {
-		numsHash[nums[i]] = numsHash[nums[i]] + 1 || 1
+  // iterate nums array
+  for (let i = 0; i < nums.length; i++) {
+    // populate hash, if the array element exist in the hash, increment by 1, otherwise assign the value of 1
+    numsHash[nums[i]] = numsHash[nums[i]] + 1 || 1;
 
-		// find existing majority element
-		if (numsHash[nums[i]] > nums.length > 2) return nums[i]
-	}
-
+    // find existing majority element
+    if (numsHash[nums[i]] > nums.length > 2) return nums[i];
+  }
 };
 
 console.log(majorityElement([3, 2, 3])); // 3
