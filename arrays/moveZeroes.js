add71ee5570a8911819eab8dd5f2157bba5302b4
Ken Austria <kenaustriadev@gmail.com>
2022-05-11 22:46:48 +0900

    moveZeroes time=O(n) space=O(1)

--- arrays/moveZeroes.js
+++ arrays/moveZeroes.js
@@ -1,24 +1,28 @@
+// Technique:
 // Runtime: 84ms
-// Time complexity: O(n)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 // Memory: 40.5MB
-const moveZeroes = (nums) => {
-  let index = 0 // declare index to keep track of
+const moveZeroes = nums => {
+  // to keep track of where to place a variable if it's value is not a 0
+  // initially we'll start placing a value at 0th index
+  let index = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] != 0) {
-      // set current i value to the current index, increment on every iteration
-      nums[index++] = nums[i]
-    }
+    // if the current nums[i] value is not 0, set nums[i]'s value at nums[index]
+    // nums[index++] commands to increment after every iteration
+    if (nums[i] != 0) nums[index++] = nums[i];
   }
 
-  // iterate nums starting at where index left of from first loop
+  // fill the rest of the array with 0s
+  // iterate nums starting at where index left off from first for loop
   for (let j = index; index < nums.length; index++) {
     // assign 0 as value to the index of where we left of from first loop
-    nums[index] = 0
+    nums[index] = 0;
   }
 
-  return nums
+  return nums;
 };
 
-console.log(moveZeroes([0,1,0,3,12])) // [1,3,12,0,0]
-console.log(moveZeroes([0])) // [0]
+console.log(moveZeroes([0, 1, 0, 3, 12])); // [1,3,12,0,0]
+console.log(moveZeroes([0])); // [0]
