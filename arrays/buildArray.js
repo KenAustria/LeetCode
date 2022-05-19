41d0e28a6e85918cb4bdcb28785a1ad2b26a5fde
Ken Austria <kenaustriadev@gmail.com>
2022-05-19 23:25:15 +0900

    buildArray time=O(n) space=O(1)

--- arrays/buildArray.js
+++ arrays/buildArray.js
@@ -1,9 +1,10 @@
 // Runtime: 92ms
-// Time complexity: O(n)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
 // Memory: 42.2MB
 const buildArray = () => {
-	return nums.map(i => nums[i]);
-}
+  return nums.map(i => nums[i]);
+};
 
-console.log(buildArray([0,2,1,5,3,4])); // [0,1,2,4,5,3]
-console.log(buildArray([5,0,1,2,3,4])) // [4,5,0,1,2,3]
+console.log(buildArray([0, 2, 1, 5, 3, 4])); // [0,1,2,4,5,3]
+console.log(buildArray([5, 0, 1, 2, 3, 4])); // [4,5,0,1,2,3]
