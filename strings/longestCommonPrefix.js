026b67723bcd6a050b738c7bce9e90ee7d21ea6d
Ken Austria <kenaustriadev@gmail.com>
2022-06-07 23:58:44 +0900

    longestCommonPrefix

---
+++ strings/longestCommonPrefix.js
@@ -0,0 +1,15 @@
+const longestCommonPrefix = strs => {
+  'use strict';
+  if (strs === undefined || strs.length === 0) {
+    return '';
+  }
+
+  return strs.reduce((prev, next) => {
+    let i = 0;
+    while (prev[i] && next[i] && prev[i] === next[i]) i++;
+    return prev.slice(0, i);
+  });
+};
+
+console.log(longestCommonPrefix(['flower', 'flow', 'flight'])); // "fl"
+console.log(longestCommonPrefix(['dog', 'racecar', 'car'])); // ""
