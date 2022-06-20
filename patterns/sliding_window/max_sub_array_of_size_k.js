92281b8a781a4b0c401c3d8e6a424ed4ae447ae0
Ken Austria <kenaustriadev@gmail.com>
2022-06-20 19:56:41 +0900

    max_sub_array_of_size_k time=O(N) space=O(1)

---
+++ patterns/sliding_window/max_sub_array_of_size_k.js
@@ -0,0 +1,60 @@
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+
+const max_sub_array_of_size_k = (k, arr) => {
+  let maxSum = 0,
+    windowSum = 0,
+    windowStart = 0;
+
+  for (window_end = 0; window_end < arr.length; window_end++) {
+    // add the next element
+    windowSum += arr[window_end];
+
+    // slide the window, no need to slide if we've not hit the window size of 'k'
+    if (window_end >= k - 1) {
+      maxSum = Math.max(maxSum, windowSum);
+      // subtract the element going out
+      windowSum -= arr[windowStart];
+      // slide the window ahead
+      windowStart += 1;
+    }
+  }
+  return maxSum;
+};
+
+/*
+Brute Force
+Time Complexity: O(N * K)
+*/
+
+// const max_sub_array_of_size_k = (k, arr) => {
+//   // declare variables
+//   let maxSum = 0,
+//     windowSum = 0;
+
+//   // iterate window
+//   for (i = 0; i < arr.length - k + 1; i++) {
+//     // reset window sum
+//     windowSum = 0;
+
+//     for (j = i; j < i + k; j++) {
+//       // accumulate sum
+//       windowSum += arr[j];
+//     }
+
+//     // assign maximum sum
+//     maxSum = Math.max(maxSum, windowSum);
+//   }
+//   return maxSum;
+// };
+
+console.log(
+  `Maximum sum of a subarray of size K: ` +
+    max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])
+); // 9
+console.log(
+  `Maximum sum of a subarray of size K: ` +
+    max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])
+); // 7
