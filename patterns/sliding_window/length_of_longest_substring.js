b35109465ce5b2fd1efd5a67f3bd8031021ea28f
Ken Austria <kenaustriadev@gmail.com>
2022-08-04 20:29:49 +0900

    length_of_longest_substring time=O(N) space=O(1)

--- patterns/sliding_window/length_of_longest_substring.js
+++ patterns/sliding_window/length_of_longest_substring.js
@@ -3,43 +3,36 @@ Time Complexity: O(N)
 Space Complexity: O(1)
 */
 
-const length_of_longest_substring = (str, k) => {
+const length_of_longest_substring = (arr, k) => {
   // declare variables
   let windowStart = 0,
     maxLength = 0,
-    maxRepeatLetterCount = 0,
-    frequencyMap = {};
+    maxOnesCount = 0;
 
-  // try to extend the range [windowStart, windowEnd]
-  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
-    const rightChar = str[windowEnd];
-    if (!(rightChar in frequencyMap)) {
-      frequencyMap[rightChar] = 0;
+  // try to extend the range (windowStart, windowEnd)
+  for (windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    // if current element is a 1
+    if (arr[windowEnd] === 1) {
+      maxOnesCount += 1;
     }
-    frequencyMap[rightChar] += 1;
 
-    // remember the maximum repeated letter count so far
-    maxRepeatLetterCount = Math.max(
-      maxRepeatLetterCount,
-      frequencyMap[rightChar]
-    );
-
-    // current window size is from windowStart to windowEnd, overall we have a letter
-    // which is repeating 'maxRepeatLetterCount' times, this means we can have a window
-    //  which has one letter repeating 'maxRepeatLetterCount' times and the remaining
-    // letters we should replace. If the remaining letters are more than 'k', it is the
-    // time to shrink the window as we are not allowed to replace more than 'k' letters
-    if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
-      leftChar = str[windowStart];
-      frequencyMap[leftChar] -= 1;
+    // shrink the window if the remaining 0s are more than "k"
+    if (windowEnd - windowStart + 1 - maxOnesCount > k) {
+      // don't include this element in maxOnesCount if remaining 0s exceed "k"
+      if (arr[windowStart] === 1) {
+        maxOnesCount -= 1;
+      }
+      // move on to next element
       windowStart += 1;
     }
 
     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
   }
+
   return maxLength;
 };
 
-console.log(length_of_longest_substring('aabccbb', 2)); // 5
-console.log(length_of_longest_substring('abbcb', 1)); // 4
-console.log(length_of_longest_substring('abccde', 1)); // 3
+console.log(length_of_longest_substring([0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], 2)); // 6
+console.log(
+  length_of_longest_substring([0, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1], 3)
+); // 9
