1b8853101ae1bd4bae7bf41ad982d3576ccaeaaa
Ken Austria <kenaustriadev@gmail.com>
2023-01-17 15:57:11 +0900

    implementations filter

--- implementations/filter.js
+++ implementations/filter.js
@@ -1,15 +1,18 @@
 Array.prototype.myFilter = function (callbackFn, thisArg) {
 	// initialize variables
-	const res = []
+	const res = [];
 
 	for (let i = 0; i < this.length; i++) {
 		// cache value in case the callback fn modifies it
-		const iValue = this[i]
-		// if the callback evaluates to true, push the el into the res
-		if (Object.hasOwn(this, i) && callbackFn.call(thisArg, iValue, i, this)) {
-			results.push(iValue)
+		const element = this[i];
+		if (
+			// ignore index if value is not defined for index (e.g. in sparse arrays).
+			Object.hasOwn(this, i) &&
+			callbackFn.call(thisArg, element, i, this)
+		) {
+			res.push(element);
 		}
 	}
 
-	return res
+	return res;
 };
