5bbc5f05415df19b25edf802228946dc91da2b36
Ken Austria <kenaustriadev@gmail.com>
2023-01-17 16:57:21 +0900

    implementations map

--- implementations/map.js
+++ implementations/map.js
@@ -1,12 +1,12 @@
-Array.prototype.map = function (callbackFn, thisArg) {
+Array.prototype.myMap = function (callbackFn, thisArg) {
 	// initialize variables
 	const len = this.length
 	// do not mutate. create new arr instead
 	const arr = new Array(len)
 
-	for (let i = 0; i < this.length; i++) {
+	for (let i = 0; i < len; i++) {
 		// call the callback on each arr el with parameters
-		if (Object.hasOwn(this, k)) {
+		if (Object.hasOwn(this, i)) {
 			arr[i] = callbackFn.call(thisArg, this[i], i, this)
 		}
 	}
