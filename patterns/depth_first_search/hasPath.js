df5ec3b9efb9d511cb17f6e0f57d21ccec0f24df
Ken Austria <kenaustriadev@gmail.com>
2023-02-22 14:19:35 +0900

    hasPath Time: O(n), Space: O(h)

--- patterns/depth_first_search/hasPath.js
+++ patterns/depth_first_search/hasPath.js
@@ -1,19 +1,23 @@
-// Time: O(N), Space: O(N)
+// Time: O(n), Space: O(h)
 
+// This function checks if there is a path from the root of a binary tree to a leaf node
+// where the sum of node values along the path is equal to a given target sum
 const hasPath = (root, sum) => {
-	if (root === null) {
-		return false;
-	}
+	// If the root is null, there is no path, so return false
+	if (!root) return false;
 
-	// if the current node is a leaf and its value is equal to the sum, we've found a path
-	if (root.val === sum && root.left === null && root.right === null) {
-		return true;
+  
+	// If the current node is a leaf node and its value is equal to the remaining sum,
+	// we have found a path, so return true
+	if ((root.left === null) && (root.right === null) && (root.val === sum)) {
+	  return true;
 	}
-
-	// recursively call to traverse the left and right sub-tree
-	// return true if any of the two recursive call return true
+  
+	// Otherwise, recursively check if there is a path in the left or right subtree
+	// that sums to the remaining target sum after subtracting the current node's value
 	return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
-}
+  };
+  
 
 console.log(`Tree has path: ${hasPath(root, 23)}`);
 console.log(`Tree has path: ${hasPath(root, 16)}`);
