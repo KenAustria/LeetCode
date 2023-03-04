77643479b9340ac2bacd6233a78a9fb86827060a
Ken Austria <kenaustriadev@gmail.com>
2023-03-04 14:40:18 +0900

    levelOrder Time = O(n), Space = O(n)

--- data_structures/binary_tree/levelOrder.js
+++ data_structures/binary_tree/levelOrder.js
@@ -63,3 +63,73 @@ const levelOrder = (root) => {
     return result;
   };
   
+/*
+HOW VALUES CHANGE WITH EACH ITERATION
+  The levelOrder function takes the input root and initializes an empty result array to store the level-order traversal. It also
+initializes a queue array to hold the nodes of the tree that need to be processed.
+    root = [3, 9, 20, null, null, 15, 7]
+    result = []
+    queue = []
+
+    The levelOrder function checks if the input root is null. If it is, it returns an empty array because there are no nodes to traverse.
+    root = [3, 9, 20, null, null, 15, 7]
+    result = []
+    queue = []
+
+    iteration 1:
+    node = 3
+    result = [[3]]
+    queue = [9, 20]
+
+    iteration 2:
+    node = 9
+    result = [[3], [9]]
+    queue = [20, 15, 7]
+
+    iteration 3:
+    node = 20
+    result = [[3], [9, 20]]
+    queue = [15, 7]
+
+    iteration 4:
+    node = 15
+    result = [[3], [9, 20], [15]]
+    queue = [7]
+
+    iteration 5:
+    node = 7
+    result = [[3], [9, 20], [15, 7]]
+    queue = []
+
+    If the input root is not null, the levelOrder function adds the input root to the queue and enters a while loop to process each node
+in the queue until it is empty. The while loop pops the first node off the queue and adds its value to the result array. It then checks
+if the node has left and right children and, if it does, adds them to the queue for processing in the next iteration. The while loop
+continues until the queue is empty, which means all nodes have been processed and added to the result array.
+    iteration 1:
+    node = 3
+    result = [[3]]
+    queue = [9, 20]
+
+    iteration 2:
+    node = 9
+    result = [[3], [9]]
+    queue = [20, 15, 7]
+
+    iteration 3:
+    node = 20
+    result = [[3], [9, 20]]
+    queue = [15, 7]
+
+    iteration 4:
+    node = 15
+    result = [[3], [9, 20], [15]]
+    queue = [7]
+
+    iteration 5:
+    node = 7
+    result = [[3], [9, 20], [15, 7]]
+    queue = []
+
+    Therefore, the level-order traversal of the input binary tree root = [3,9,20,null,null,15,7] is [[3], [9, 20], [15, 7]]. The values
+of each node are processed in the order 3, 9, 20, 15, 7, and added to the result array in the order [3], [9, 20], [15, 7].
+*/
