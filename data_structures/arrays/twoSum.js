cbd9c8f9bbd604d0e61c515d2836aead8a46ca9e
Ken Austria <kenaustriadev@gmail.com>
2023-03-08 18:40:00 +0900

    data_structures/arrays/twoSum.js

--- data_structures/arrays/twoSum.js
+++ data_structures/arrays/twoSum.js
@@ -1,65 +1,57 @@
 /*
-Strategy: One Pass Hash Map
-Runtime: 138ms
-Time Complexity: O(n)
-Space Complexity: O(n)
-Memory: 43.4MB
+EXPLANATION
+  The function takes in two parameters, an array of integers nums and a target value target. It returns an array of two indices that
+correspond to the positions of the two numbers in the array that add up to the target value.
+
+  The function makes use of an object complements to store the complements of the numbers in the array. A complement of a number is
+the difference between the target value and the number itself. The function then checks if the complement of the current number exists
+in the complements object. If it does, the function returns an array of the indices of the two numbers that add up to the target value.
+
+  The function iterates over the array using a for loop, starting from the first number at index 0 and ending at the last number at index
+nums.length - 1. In each iteration, the function calculates the complement of the current number by subtracting it from the target value.
+It then checks if the complement exists in the complements object using the if statement. If the complement exists, the function returns
+an array of the indices of the two numbers that add up to the target value, using the index of the complement and the index of the current
+number.
+
+  If the complement does not exist, the function adds the current number and its index to the complements object. This ensures that the
+complement of any future number in the array will be checked against the previously seen numbers in the complements object. By storing the
+indices of the numbers in the complements object, the function can quickly retrieve the indices of the two numbers that add up to the
+target value when it finds a complement.
 */
 
+/*
+TIME AND SPACE COMPLEXITY
+  Time = O(n), Space = O(n)
+  The time complexity of this function is O(n) because it has a single loop that iterates through the nums array once. The operations
+inside the loop, which include accessing and assigning values in the complements object, are constant time operations.
+
+  The space complexity of this function is also O(n) because in the worst case scenario, all the elements of the nums array could be stored
+in the complements object. However, the space used by the complements object could be smaller if the input array has duplicates or there
+is a solution to the problem earlier in the array.
+*/
+
+// Define a function called twoSum which takes in an array of integers called nums and an integer called target as its parameters
 const twoSum = (nums, target) => {
-  // declare host hash
-  let seenHash = {};
+  // Define an empty object called complements that will store the complements needed to reach the target sum
+  const complements = {};
 
-  // iterate loop
-  for (let i = 0; i < nums.length; i += 1) {
-    // diff is the second number to lookup in the object
-    let diff = target - nums[i];
-
-    /* while iterating and inserting elements into hash map,
-    ..also check if current element's complement already exist in the hash map */
-    if (seenHash[diff] !== undefined) {
-      // if exist, return
-      return [i, seenHash[diff]];
-      // else, map array element to its index
-    } else {
-      seenHash[nums[i]] = i;
-    }
+  // Start a for loop that will iterate over the length of the nums array
+  for (let i = 0; i < nums.length; i++) {
+      // Define a variable called complement that stores the difference between the target and the current element in the nums array
+      const complement = target - nums[i];
+      
+      // If the current complement already exists in the complements object
+      if (complements[complement] !== undefined) {
+          // Return an array with the index of the current complement and the index of the current element as its two elements
+          return [complements[complement], i];
+      }
+      
+      // Add the current element to the complements object with its index as its value
+      complements[nums[i]] = i;
   }
 };
 
 /*
-Complexity Analysis:
-Time Complexity : O(n)
-We iterate the array containing n elements only once. Each look up in the hash costs only O(1) time.
+HOW THE VALUES CHANGE IN EACH ITERATION
 
-Space Complexity: O(n)
-The extra space required depends on the number of items stored in the hash, which stores at most n elements.
 */
-
-/*
-Strategy: Two Pass Hash Map
-const twoSum = (nums, target) => {
-  // declare host hash
-  let numsIndices = {};
-
-  // populate host hash, map each array element to its index
-  nums.forEach((num, index) => {
-    numsIndices[num] = index;
-  });
-
-  // initiate loop
-  for (let i = 0; i < nums.length; i += 1) {
-    // diff is the second number to lookup in the object
-    let diff = target - nums[i];
-
-    // if diff exist in our object AND it isn't our current index to prevent duplicates
-    if (numsIndices[diff] && numsIndices[diff] !== i) {
-      return [i, numsIndices[diff]];
-    }
-  }
-};
-*/
-
-console.log(twoSum([3, 2, 4], 6)); // [1, 2]
-console.log(twoSum([6, 2, 3, 9, -5, 5, 7, 2], 1)); // [0, 4]
-console.log(twoSum([2, -3, 1, -5], -3)); // [0, 3]
--- data_structures/strings/groupAnagrams.js
+++ data_structures/strings/groupAnagrams.js
@@ -1,30 +1,101 @@
-// Runtime: 120ms
-// Time complexity: O(n)
-// Memory: 49.4MB
-const groupAnagrams = function(strs) {
-  let host_map = {};
-  let result_arr = [];
+/*
+EXPLANATION
+  The groupAnagrams function takes an array of strings strs as input and returns an array of arrays where each sub-array contains
+strings that are anagrams of each other. Anagrams are words or phrases formed by rearranging the letters of a different word or phrase.
+In this function, we create an object called anagrams that will hold the sorted strings as keys and an array of their anagrams as values.
 
-  for (let i = 0; i < strs.length; i++) {
-    let str = strs[i]; // unsorted version used to be pushed to result_arr
-    let sorted = str.split('').sort().join('');
+  The function then loops through each string in the input array using a for...of loop. For each string, we sort its characters in
+alphabetical order using the split(), sort(), and join() methods and assign the sorted string to the sortedStr variable. We then check if
+the sorted string already exists as a key in the anagrams object. If it does, we add the current string to the array of anagrams for that
+key. If it does not, we create a new key with the sorted string and assign an array containing the current string as its value.
 
-		// if sorted word doesn't exist in host_map, populate as an array, first of it's group
-		// else, add as addition to group
-    if (host_map[sorted] === undefined) {
-      host_map[sorted] = [str];
-    } else {
-      host_map[sorted].push(str);
-    }
+  After looping through all the strings in the input array, we return an array of arrays using the Object.values() method. This method
+returns an array of all the values in the anagrams object, which are arrays of anagrams. By doing this, we have successfully grouped all
+the anagrams together in separate sub-arrays.
+
+  This implementation is efficient because we only loop through the input array once and use a constant amount of additional space to
+store the anagrams in the anagrams object. The use of the Object.values() method makes it easy to return the arrays of anagrams as the
+final result. Overall, this function is a good example of how to efficiently group strings that are anagrams of each other.
+*/
+
+/*
+TIME AND SPACE COMPLEXITY
+  The time complexity of the groupAnagrams function is O(n * k log k), where n is the number of strings in the strs array and k is the
+maximum length of a string in strs. The sorting operation inside the loop takes O(k log k) time, and this operation is performed for each
+of the n strings in strs. The worst-case time complexity would be O(n * k^2 log k) if all strings are the same length k.
+
+  The space complexity of the function is O(n * k), where n is the number of strings in the strs array and k is the maximum length of a
+string in strs. This is because the anagrams object can store up to n keys (one for each unique sorted string) and each value is an array
+that can hold up to k strings. Therefore, the worst-case space complexity would be O(n^2) if all strings are unique and of the same length.
+*/
+
+// Define a function that takes an array of strings as input
+const groupAnagrams = (strs) => {
+  // Create an empty object to store anagrams
+  const anagrams = {};
+
+  // Iterate through each string in the input array
+  for (const str of strs) {
+      // Sort the current string, convert it to a string, and assign to sortedStr variable
+      const sortedStr = str.split('').sort().join('');
+
+      // If the sorted string already exists in the anagrams object, push the current string to its array value
+      if (anagrams[sortedStr]) {
+          anagrams[sortedStr].push(str);
+      } 
+      // Otherwise, add the sorted string to the object as a key and set its value to an array containing the current string
+      else {
+          anagrams[sortedStr] = [str];
+      }
   }
-
-  for (let arr in host_map) {
-    result_arr.push(host_map[arr]);
-  }
-
-  return result_arr;
+  // Return an array of the values in the anagrams object
+  return Object.values(anagrams);
 };
 
-console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"])); // [["bat"],["nat","tan"],["ate","eat","tea"]]
-console.log(groupAnagrams([""])); // [[""]]
-console.log(groupAnagrams(["a"])); // [["a"]]
+/*
+HOW THE VALUES CHANGE IN EACH ITERATION
+  Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+  anagrams: { e: ["eat"] }
+  str: "eat"
+  sortedStr: "aet"
+  anagrams[sortedStr]: undefined
+  [str]: ["eat"]
+
+  Iteration 2:
+  anagrams: { aet: ["eat", "tea"] }
+  str: "tea"
+  sortedStr: "aet"
+  anagrams[sortedStr]: ["eat", "tea"]
+  [str]: ["tea"]
+
+  Iteration 3:
+  anagrams: { aet: ["eat", "tea"], ant: ["tan"] }
+  str: "tan"
+  sortedStr: "ant"
+  anagrams[sortedStr]: undefined
+  [str]: ["tan"]
+
+  Iteration 4:
+  anagrams: { aet: ["eat", "tea", "ate"], ant: ["tan"] }
+  str: "ate"
+  sortedStr: "aet"
+  anagrams[sortedStr]: ["eat", "tea", "ate"]
+  [str]: ["ate"]
+
+  Iteration 5:
+  anagrams: { aet: ["eat", "tea", "ate"], ant: ["tan", "nat"] }
+  str: "nat"
+  sortedStr: "ant"
+  anagrams[sortedStr]: ["tan", "nat"]
+  [str]: ["nat"]
+
+  Iteration 6:
+  anagrams: { aet: ["eat", "tea", "ate"], ant: ["tan", "nat"], abt: ["bat"] }
+  str: "bat"
+  sortedStr: "abt"
+  anagrams[sortedStr]: undefined
+  [str]: ["bat"]
+
+  At the end of the loop, Object.values(anagrams) is [['eat','tea','ate'],['tan','nat'],['bat']]
+*/
